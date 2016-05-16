9af89ed2b5acd1a71334179f357f8cbb4e1691af
Rafael Felix <rfelixg@gmail.com>
2016-05-15 19:38:31 -0700

    beta 1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,47 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+$(document).on('ready', function(){
+
+  $('#order-form').validate({
+    submitHandler: function(form) {
+
+      form.submit();
+    },
+
+    rules: {
+      "your-name": {
+        required: true,
+        maxlength: 128,
+        digits: false
+      },
+      "your-city": {
+        required: true,
+      },
+      "your-state": {
+        required: true,
+        maxlength: 2
+      },
+      "your-zip": {
+        required: true,
+        maxlength: 5,
+        digits: true
+      },
+      "card-holder-name": {
+        required: true,
+        maxlenght: 128,
+      },
+      "card-number": {
+        required: true,
+        creditcard: true
+      },
+      "ccv": {
+        required: true,
+        maxlength: 3,
+        digits: true
+      }
+    }
+  });
+
+});
